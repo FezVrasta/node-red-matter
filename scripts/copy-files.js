a902a41f7dc0fbc096584dc549e09446ff8f06e0
Federico Zivolo <federico.zivolo@gmail.com>
2023-08-28 15:53:40 +0200

    feat: matter icon and fix alignment

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -1,13 +1,22 @@
 const glob = require('glob');
 const ncp = require('ncp').ncp;
+const path = require('path');
+const fs = require('fs');
 
 // Source and destination paths
-const sourceDir = 'src/**/*.html';
+const sourceDir = 'src/**/*.{html,svg}';
 const destinationDir = 'lib/src';
 
 // Copy HTML files
 glob.sync(sourceDir).forEach((file) => {
   const destinationFile = file.replace(/^src\//, `${destinationDir}/`);
+
+  // create directory recursively if it doesn't exist
+  const destinationDirname = path.dirname(destinationFile);
+  if (!fs.existsSync(destinationDirname)) {
+    fs.mkdirSync(destinationDirname, { recursive: true });
+  }
+
   ncp(file, destinationFile, (err) => {
     if (err) {
       console.error(`Error copying ${file}:`, err);
